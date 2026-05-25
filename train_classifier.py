2ce35c3e8767024059c4a553732a2bb0ba70df41
UltralyticsAssistant <web@ultralytics.com>
2026-05-25 05:13:40 +0000

    Auto-format by https://ultralytics.com/actions

--- train_classifier.py
+++ train_classifier.py
@@ -1,31 +1,29 @@
 from ultralytics import YOLO
-import torch
-import os
 
 # 1. ĐƯỜNG DẪN ĐẾN DATASET
 # Thay 'path/to/your/dataset' bằng đường dẫn thực tế đến thư mục bạn đã tải về
 # Ví dụ: 'C:/Users/Admin/Downloads/football-dataset'
-dataset_path = 'dataset/football-player.v5i.yolo26/data.yaml'
+dataset_path = "dataset/football-player.v5i.yolo26/data.yaml"
 
 # 2. KHỞI TẠO MÔ HÌNH (Dòng Nano cực nhẹ cho RTX 3050)
 # Bạn có thể dùng 'yolov8n.pt' hoặc 'yolo11n.pt' (bản mới nhất)
 
-if __name__ == '__main__':
-    model = YOLO('yolo26n.pt') 
+if __name__ == "__main__":
+    model = YOLO("yolo26n.pt")
 
     # 3. CẤU HÌNH VÀ HUẤN LUYỆN
     # Tối ưu cho 4GB VRAM để tránh lỗi Out of Memory (OOM)
     results = model.train(
-        data=dataset_path, 
-        epochs=100,           # Số vòng lặp huấn luyện
-        imgsz=640,            # Kích thước ảnh chuẩn (640x640)
-        batch=8,              # RTX 3050 4GB nên chạy batch 8 hoặc 16
-        workers=4,            # Tận dụng 32GB RAM để load ảnh nhanh hơn
-        device=0,             # Chạy bằng GPU (0 là ID của card đồ họa)
-        amp=True,             # Bật chế độ tự động tối ưu bộ nhớ (Automatic Mixed Precision)
-        project='football_detection', # Tên dự án
-        name='yolo_player_model',     # Tên phiên bản training
-        exist_ok=True
+        data=dataset_path,
+        epochs=100,  # Số vòng lặp huấn luyện
+        imgsz=640,  # Kích thước ảnh chuẩn (640x640)
+        batch=8,  # RTX 3050 4GB nên chạy batch 8 hoặc 16
+        workers=4,  # Tận dụng 32GB RAM để load ảnh nhanh hơn
+        device=0,  # Chạy bằng GPU (0 là ID của card đồ họa)
+        amp=True,  # Bật chế độ tự động tối ưu bộ nhớ (Automatic Mixed Precision)
+        project="football_detection",  # Tên dự án
+        name="yolo_player_model",  # Tên phiên bản training
+        exist_ok=True,
     )
 
     print("--- HUẤN LUYỆN HOÀN TẤT ---")
